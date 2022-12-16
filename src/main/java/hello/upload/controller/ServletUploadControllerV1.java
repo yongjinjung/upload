f355eae330f458185864addfe2b45ffdbdc794a0
YongJin Jung <jyj1641@gmail.com>
2022-12-16 19:16:33 +0900

    프로젝트 생성

---
+++ src/main/java/hello/upload/controller/ServletUploadControllerV1.java
@@ -0,0 +1,11 @@
+package hello.upload.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/servlet/v1")
+public class ServletUploadControllerV1 {
+}
